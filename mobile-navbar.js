f188252a38424a5c05b303bc34935bf6d8933150
Igor Santos <125512321+igorzoca@users.noreply.github.com>
2023-07-13 17:50:29 -0300

    13.07.23.1

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -4,10 +4,26 @@ class MobileNavBar {
         this.navList =  document.querySelector(navList)
         tihis.navLinks =  document.querySelectorAll(navLinks)
         this.activeClass = "active"
+
+        this.handleClick.bind(this)
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link) => {
+            link.style.animation
+            ? (link.style.animation = " ")
+            : (link.style.animation = 'navLinkFade 0.5s case forwards 0.3s');
+        });
+    }
+
+    handleClick() {
+        console.log(this)
+        this.navList.classList.toggle(this.activeClass);
+        this.animateLinks();
     }
 
     addClickEvent() {
-        this.mobileMenu.addEventListener("click", () => console.log(heyy));
+        this.mobileMenu.addEventListener("click", this.handleClick);
     }
 
     Init() {
